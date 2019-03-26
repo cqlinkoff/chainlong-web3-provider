b515098028c451a6e5dcb7e6a51184e5bba364ce
kagawagao <kingsongao1221@gmail.com>
2019-03-26 18:57:45 +0800

    feat: update provider

--- src/hooks.js
+++ src/hooks.js
@@ -1,14 +1,40 @@
 import WKBridge from '@cqlinkoff/wk-bridge'
 export default class WalletHooks {
-  constructor () {
+  constructor (address) {
     this.bridge = new WKBridge({
       namespace: 'dApp'
     })
+    this.accounts = [address]
+  }
+  serializeResponse = (result) => {
+    const data = {
+      jsonrpc: '2.0'
+    }
+    if (typeof result === 'object' && result.jsonrpc && result.result) {
+      data.result = result.result
+    } else {
+      data.result = result
+    }
+    return data
+  }
+  getAccounts = (cb) => {
+    cb(null, this.accounts)
   }
   signTransaction = (msg, cb) => {
     this.bridge.postMessage('signTransaction', msg)
-      .then(txHash => cb(null, txHash))
-      .catch(err => cb(err))
+      .then(txHash => {
+        // const data = this.serializeResponse(txHash)
+        // console.log(data)
+        cb(null, txHash)
+      })
+      .catch(err => {
+        console.log(err)
+        cb(err)
+      })
+  }
+  publishTransaction = (msg, cb) => {
+    console.log(msg)
+    cb(null, msg)
   }
   signMessage = (cb) => {}
   signPersonalMessage = (cb) => {}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import * as ProviderEngine from 'web3-provider-engine'
 import * as RpcSubProvider from 'web3-provider-engine/subproviders/rpc'
 import * as HookedWalletSubProvider from 'web3-provider-engine/subproviders/hooked-wallet'
-import WalletHooks from './hooks'
+import * as SubscriptionsSubProvider from 'web3-provider-engine/subproviders/subscriptions'
 import * as Web3 from 'web3'
+import WalletHooks from './hooks'
 
 export default class ChainLongWeb3Provider {
   constructor (options = {}) {
-    const {
-      rpcUrl
-    } = options
+    const { rpcUrl, address } = options
     this.engine = new ProviderEngine()
-    const hookedWallet = new HookedWalletSubProvider(new WalletHooks())
+    const hookedWallet = new HookedWalletSubProvider(new WalletHooks(address))
     this.engine.addProvider(hookedWallet)
+    this.engine.addProvider(new SubscriptionsSubProvider())
     if (rpcUrl) {
-      this.engine.addProvider(new RpcSubProvider({
-        rpcUrl
-      }))
+      this.engine.addProvider(
+        new RpcSubProvider({
+          rpcUrl
+        })
+      )
     }
     this.engine.start()
     this.web3 = new Web3(this.engine)
